7a2607b4c9fe0b55a288c76edd25d8fd3c07135d
Riku Korkiasaari <74648156+KorkiasaariRiku@users.noreply.github.com>
2021-11-10 12:33:42 +0200

    Added basic routing
    Now we have seperate Header and content area. Also, routing.

--- woltora/src/App.js
+++ woltora/src/App.js
@@ -1,11 +1,20 @@
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <p>Init</p>
-    </div>
+    <BrowserRouter>
+      <div>
+        <div className="navbar">
+          <Link to="/"><div>Home</div></Link>
+          <Link to="/contacts"><div>Contact List</div></Link>
+        </div>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
