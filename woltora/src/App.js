0313f696e3db188fa94fba1352dc9a65fe256dec
Riku Korkiasaari <74648156+KorkiasaariRiku@users.noreply.github.com>
2021-11-11 13:36:35 +0200

    Crude user login / register
    Basic frontend for user login / register, missing backend, since the lecture hasn't been held yet.

--- woltora/src/App.js
+++ woltora/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Owner from './Components/Owner';
+import Register from './Components/Register';
 
 function App() {
   return (
@@ -10,12 +11,13 @@ function App() {
       <div>
         <div className="navbar">
           <Link to="/"><div>Home</div></Link>
-          <Link to="/Login"><div>Login</div></Link>
+          <Link to="/Login"><div>Login/Register</div></Link>
         </div>
         <Routes>
           <Route path="/" element={ <Home /> } />
           <Route path="/Login" element={ <Login /> } />
           <Route path="/Owner" element={ <Owner /> } />
+          <Route path="/Login/Register" element={ <Register /> } />
         </Routes>
       </div>
     </BrowserRouter>
