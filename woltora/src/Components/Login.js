23ad9ddfa4757820b8e7dddcfb19c3601a3b55cf
Riku Korkiasaari <74648156+KorkiasaariRiku@users.noreply.github.com>
2021-12-09 18:01:40 +0200

    Login works without any real function tho.

--- woltora/src/Components/Login.js
+++ woltora/src/Components/Login.js
@@ -1,31 +1,52 @@
-import React from 'react'
+import React from 'react';
 import styles from './Styles/Login.module.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Login() {
-    return (
-      <div className={styles.loginBackground}>
-        <div className={styles.loginHeader}>
-          <h1>Login here or <Link to="/Register"><div>Register</div></Link></h1>
-        </div>
-        <div className={styles.loginBox}>
-        <form>
-          <div><label>
-          <input type="text" placeholder="Username" />
-          </label>
-          </div>
-          <div>
-          <label>
-          <br/>
-          <input type="password" placeholder="Password"/>
-          </label>
-          <div>
-          <br/>
-          <input type="submit" placeholder="Submit" />
-          </div>
-          </div>
-          </form>
-        </div>
-      </div>
-    )
+  const username = React.useRef(null)
+  const password = React.useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendLogin();
   }
+  const sendLogin = async () => {
+    try {
+      const res = await axios.get('http://localhost:4000/login', {
+        auth: {
+          username: username.current.value,
+          password: password.current.value
+        }
+      })
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return (
+    <div className={styles.loginBackground}>
+      <div className={styles.loginHeader}>
+        <h1>Login here or <Link to="/Register"><div>Register</div></Link></h1>
+      </div>
+      <div className={styles.loginBox}>
+        <form onSubmit={handleSubmit}>
+          <div><label>
+            <input type="text" placeholder="Username" ref={username} />
+          </label>
+          </div>
+          <div>
+            <label>
+              <br />
+              <input type="password" placeholder="Password" ref={password} />
+            </label>
+            <div>
+              <br />
+              <input type="submit" placeholder="Submit" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
